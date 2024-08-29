74e532ad8d5a9bb2a9b3214ddb9f4686631a6b12
abhimanyu <abhimanyupallavisudhir@gmail.com>
2024-08-29 01:48:55 +0100

    let decorator take arguments

--- costly/decorator.py
+++ costly/decorator.py
@@ -1,43 +1,43 @@
 from functools import wraps
+from typing import Callable
 from costly.costlog import Costlog
 from costly.simulators.llm_simulator_faker import LLM_Simulator_Faker
 from costly.estimators.llm_api_estimation import LLM_API_Estimation
 
 
-def costly(func):
-    @wraps(func)
-    def wrapper(
-        input_string: str,
-        model: str,
-        cost_log: Costlog = None,
-        simulate: bool = False,
-        description: list[str] = None,
-        *args,
-        **kwargs
-    ):
-        if simulate:
-            return LLM_Simulator_Faker.simulate_llm_call(
-                input_string=input_string,
-                model=model,
-                response_model=str,
-                cost_log=cost_log,
-                description=description,
-            )
+def costly(
+    simulator: Callable = LLM_Simulator_Faker.simulate_llm_call,
+    estimator: Callable = LLM_API_Estimation.get_cost_real
+):
+    def decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            cost_log = kwargs.pop('cost_log', None)
+            simulate = kwargs.pop('simulate', False)
+            description = kwargs.pop('description', None)
 
-        if cost_log is not None:
-            with cost_log.new_item() as (item, timer):
-                output_string = func(input_string, model, *args, **kwargs)
-                cost_item = LLM_API_Estimation.get_cost_real(
-                    model=model,
-                    input_string=input_string,
-                    output_string=output_string,
-                    description=description,
-                    timer=timer(),
+            if simulate:
+                return simulator(
+                    **kwargs,
+                    cost_log=cost_log,
+                    description=description
                 )
-                item.update(cost_item)
-        else:
-            output_string = func(input_string, model, *args, **kwargs)
 
-        return output_string
+            if cost_log is not None:
+                with cost_log.new_item() as (item, timer):
+                    output = func(*args, **kwargs)
+                    cost_item = estimator(
+                        **kwargs,
+                        output_string=output,
+                        description=description,
+                        timer=timer()
+                    )
+                    item.update(cost_item)
+            else:
+                output = func(*args, **kwargs)
 
-    return wrapper
+            return output
+
+        return wrapper
+
+    return decorator
