e93cee49b15388ef5ef7a914f3bfd6a6ba60fbe7
abhimanyu <abhimanyupallavisudhir@gmail.com>
2024-09-06 20:58:14 +0100

    feat: diffaults

--- README.md
+++ README.md
@@ -79,10 +79,10 @@ Tests for instructor currently fail.
 ## TODO
 
 - [ ] Make it work with async
-- [ ] Support for locally run LLMs -- ideally need a cost & time estimator that takes into account your machine details, GPU pricing etc.
 - [ ] Decide and document what the best way to "propagate" `description` (for breakdown purposes) through function calls is. Have the user manually write `def f(...): ... g(description = kwargs.get("description") + ["f"]`? Add a `@description("blabla")` decorator? Add a `@description` decorator that automatically appends the function name and arguments into `description`?
 - [x] Better solution for token counting for Chat messages (search `HACK` in the repo)
 - [x] make instructor tests pass https://community.openai.com/t/how-to-calculate-the-tokens-when-using-function-call/266573/11
+- [ ] Support for locally run LLMs -- ideally need a cost & time estimator that takes into account your machine details, GPU pricing etc.
 - [ ] support more models
 
 Instructor tests don't really pass but I can kinda live with this. Lmk if anyone has a good way to count tokens from messages that includes tool calling (I'm using [this](https://community.openai.com/t/how-to-calculate-the-tokens-when-using-function-call/266573/11)).
---
+++ costly/updatable_default.py
@@ -0,0 +1,151 @@
+from typing import Callable, Any
+from functools import wraps
+
+
+def updatable_default(updator: Callable = lambda x, y: x | y, **defaults):
+    """
+    Ever wished that instead of *overriding* a function's default
+    parameters with those supplied in your call, you could just
+    "update" it with that supplied in your call? Something like
+    f(y += 1)?
+
+    You can do this with `updatable_default`. Instead of defining the default
+    parameter of the function in the standard way, do it like this:
+
+    @updatable_default(x = {"a": 1})
+    def f(x):
+        return x
+
+    f() # returns {"a": 1}
+    f(x = {"b": 2}) # returns {"a": 1, "b": 2}
+    f(x = {"a": 3}) # returns {"a": 3}
+
+    @updatable_default(updator = lambda x, y: x + y, y = 8)
+    def f(y):
+        return y
+
+    f(3) # returns 11
+
+    def update_recursive(source, overrides):
+        for key, value in overrides.items():
+            if isinstance(value, dict) and key in source and isinstance(source[key], dict):
+                update_recursive(source[key], value)
+            elif isinstance(value, list) and key in source and isinstance(source[key], list):
+                source[key] = source[key] + value
+            else:
+                source[key] = value
+        return source
+
+
+    @updatable_default(
+        post={
+            "content": "Hello, World!",
+            "OP": {
+                "name": "John",
+                "age": 30,
+                "metadata": {
+                    "account_created": "2024-01-01",
+                    "banned": False,
+                    "votes_cast": 1,
+                },
+            },
+            "metadata": {"score": -2, "flags": ["spam"]},
+        },
+        updator=update_recursive,
+    )
+    def f(post):
+        return post
+
+
+    f(
+        post={
+            "OP": {
+                "metadata": {"account_deleted": "2024-01-02", "banned": True},
+            },
+            "metadata": {"flags": ["deleted"]},
+        },
+    )
+
+    ### returns:
+    {
+        "content": "Hello, World!",
+        "OP": {
+            "name": "John",
+            "age": 30,
+            "metadata": {
+                "account_created": "2024-01-01",
+                "banned": True,
+                "votes_cast": 1,
+                "account_deleted": "2024-01-02",
+            },
+        },
+        "metadata": {"score": -2, "flags": ["spam", "deleted"]},
+    }
+
+    Stuff this can be useful for:
+
+    - tracing
+    - cumulative configurations
+    - "biasing" or "damping" some kind of computation
+
+    The default updator is `|`.
+    """
+
+    def decorator(func: Callable):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            for arg_name, default_value in defaults.items():
+                if arg_name in kwargs:
+                    kwargs[arg_name] = updator(default_value, kwargs[arg_name])
+                else:
+                    import inspect
+
+                    sig = inspect.signature(func)
+                    param_position = list(sig.parameters.keys()).index(arg_name)
+                    if param_position < len(args):
+                        args = list(args)
+                        args[param_position] = updator(
+                            default_value, args[param_position]
+                        )
+                        args = tuple(args)
+                    else:  # if arg_name not provided, add it and set it to default
+                        kwargs[arg_name] = default_value
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+
+# def _updatable_default(updator: Callable, arg_name: str, default: Any = None):
+#     """
+#     alternate implementation of `updatable_default`
+#     @_updatable_default(updator = lambda x, y: x + y, arg_name = "y", default = 1)
+#     def f(y):
+#         return y
+
+#     f(3) # returns 4
+#     """
+
+#     def decorator(func: Callable):
+#         @wraps(func)
+#         def wrapper(*args, **kwargs):
+#             if arg_name in kwargs:
+#                 kwargs[arg_name] = updator(default, kwargs[arg_name])
+#             else:
+#                 import inspect
+
+#                 sig = inspect.signature(func)
+#                 param_position = list(sig.parameters.keys()).index(arg_name)
+#                 if param_position < len(args):
+#                     args = list(args)
+#                     args[param_position] = updator(default, args[param_position])
+#                     args = tuple(args)
+#                 else:  # if arg_name not provided, add it and set it to default
+#                     kwargs[arg_name] = default
+#             return func(*args, **kwargs)
+
+#         return wrapper
+
+#     return decorator
